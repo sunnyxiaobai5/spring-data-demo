7d2c7fc394a2905325c6c0887da72d7d7a4d06ba
sunnyxiaobai5 <sunnyxiaobai5@gmail.com>
2015-12-06 20:52:13 +0800

    1、引入itextpdf包，读取数据生成pdf文件 2、spring security简单配置

---
+++ src/main/java/com/sunnyxiaobai5/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.sunnyxiaobai5.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(
+            AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("123456").roles("USER");
+    }
+}
