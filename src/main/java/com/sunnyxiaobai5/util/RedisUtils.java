451d46a400624e7f45256b00c5cf37d311d2dcef
sunnyxiaobai5 <sunnyxiaobai5@gmail.com>
2016-04-05 17:54:39 +0800

    添加redis包

---
+++ src/main/java/com/sunnyxiaobai5/util/RedisUtils.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * sunnyxiaobai5@gmail.com
+ * <p>
+ * <li>项目名称: spring-data-demo</li>
+ * <li>完整包名: com.sunnyxiaobai5.util</li>
+ * <li>文件名称: PdfTableUtils.java</li>
+ * <li>内容摘要: </li>
+ * <li>内容描述: </li>
+ * <li>其他说明: </li>
+ * <li>@author Xiangyong Zeng</li>
+ ******************************************************************************/
+package com.sunnyxiaobai5.util;
+
+public class RedisUtils {
+
+}
