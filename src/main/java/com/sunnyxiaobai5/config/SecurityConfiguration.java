7bf2991187fb5a27fd92f4a69db18264c062a4b7
sunnyxiaobai5 <sunnyxiaobai5@gmail.com>
2016-04-07 15:28:34 +0800

    Redis配置

--- src/main/java/com/sunnyxiaobai5/config/SecurityConfiguration.java
+++ src/main/java/com/sunnyxiaobai5/config/SecurityConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(
