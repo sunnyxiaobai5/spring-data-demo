a82abee2e40037c4c0dfe75021eaa88063022d51
sunnyxiaobai5 <sunnyxiaobai5@gmail.com>
2016-04-18 00:04:14 +0800

    通过数据库登陆验证

---
+++ src/main/java/com/sunnyxiaobai5/service/UserDetailsServiceImpl.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * sunnyxiaobai5@gmail.com
+ * <p>
+ * <li>项目名称: spring-data-demo</li>
+ * <li>完整包名: com.sunnyxiaobai5.service</li>
+ * <li>文件名称: UserDetailsServiceImpl.java</li>
+ * <li>内容摘要: </li>
+ * <li>内容描述: </li>
+ * <li>其他说明: </li>
+ * <li>@author Xiangyong Zeng</li>
+ ******************************************************************************/
+package com.sunnyxiaobai5.service;
+
+import com.sunnyxiaobai5.domain.User;
+import com.sunnyxiaobai5.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Component("userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final Logger log = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
+    @Inject
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        log.debug("Authenticating {}", login);
+        String lowercaseLogin = login.toLowerCase();
+        Optional<User> userFromDatabase = userRepository.findOneByName(lowercaseLogin);
+        return userFromDatabase.map(user -> {
+
+//            List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
+//                    .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+//                    .collect(Collectors.toList());
+            return new org.springframework.security.core.userdetails.User(lowercaseLogin,
+                    user.getPassword(),
+                    new ArrayList<>());
+        }).orElseThrow(() -> new UsernameNotFoundException("User " + lowercaseLogin + " was not found in the database"));
+    }
+}
